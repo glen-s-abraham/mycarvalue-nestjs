e8f555723832e092dbf20e3c47f7802ab4def2b1
glen-s-abraham <glenprojects666@gmail.com>
2023-03-18 17:56:41 +0530

    db repos added

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Report } from './report.enitiy';
 import { ReportsController } from './reports.controller';
 import { ReportsService } from './reports.service';
 
 @Module({
   controllers: [ReportsController],
-  providers: [ReportsService]
+  providers: [ReportsService],
+  imports:[TypeOrmModule.forFeature([Report])]
 })
 export class ReportsModule {}
